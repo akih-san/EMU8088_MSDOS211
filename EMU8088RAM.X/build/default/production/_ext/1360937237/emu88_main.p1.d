8e523c9899c2b5983cb0961c4222cdbc2a308503
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-02-18 17:30:52 +0900

    Add files via upload

---
+++ EMU8088RAM.X/build/default/production/_ext/1360937237/emu88_main.p1.d
@@ -0,0 +1,10 @@
+build/default/production/_ext/1360937237/emu88_main.p1:  \
+../src/emu88_main.c  \
+../src/../src/emu88.h  \
+../src/../src/picconfig.h  \
+../src/../fatfs/ff.h  \
+../src/../fatfs/../fatfs/ffconf.h  \
+../src/../drivers/utils.h  \
+../src/../i88/iosys.inc  \
+../src/../i88/msdos.inc  \
+../src/../i88/unimon_8086.inc 
